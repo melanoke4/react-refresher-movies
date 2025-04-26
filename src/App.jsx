f705eecfb85b2b337320bb7788dfda6642c4a39d
Melanie <princessmelanoke@gmail.com>
2025-04-26 12:57:57 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import MovieCard from './componets/MovieCard'
+import Home from './pages/Home'
+
+function App() {
+
+
+
+  return (
+  <>
+  <Home />
+  </>
+  )
+}
+
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import MovieCard from "../componets/MovieCard";
+import { useState } from "react";
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "Spirited Away", release_date: "2001" },
+    { id: 2, title: "Princess Mononoke", release_date: "1997" },
+    { id: 3, title: "Howl's Moving Castle", release_date: "2004" },
+  ];
+
+  const handleSearch = () => {};
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="seacr-input"
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <div className="movies.grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
+
+// when dynamically rendering multiple things, add the key property so React can identify this component
+// compared to the others being rendered
