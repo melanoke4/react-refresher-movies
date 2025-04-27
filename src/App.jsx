ddaa777879f5bf97c901c8dbb17621ccff2607f0
Melanie <princessmelanoke@gmail.com>
2025-04-27 16:06:02 -0500

    basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-
-import './App.css'
-import MovieCard from './componets/MovieCard'
-import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+import "./css/App.css";
+import MovieCard from "./componets/MovieCard";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./componets/NavBar";
 
 function App() {
-
-
-
   return (
-  <>
-  <Home />
-  </>
-  )
+    <div>
+      <NavBar />
+      <main className="msin-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-
-export default App
+export default App;
