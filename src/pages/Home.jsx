9bf1b4dec3d56ab73ebba861093adc78ed9abd79
Melanie <princessmelanoke@gmail.com>
2025-04-27 15:32:38 -0500

    updating home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import MovieCard from "../componets/MovieCard";
 import { useState } from "react";
 
 function Home() {
-    const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const movies = [
     { id: 1, title: "Spirited Away", release_date: "2001" },
@@ -10,7 +10,11 @@ function Home() {
     { id: 3, title: "Howl's Moving Castle", release_date: "2004" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
@@ -19,6 +23,8 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="seacr-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">
           Search
